4b6ce70121675573590ee4cf43f01d8eb271e492
vongolaKT <kevin.baseball97@yahoo.com>
2024-07-23 14:24:10 -0400

    added image slideshow on community page and image assets

--- assets/js/customscripts.js
+++ assets/js/customscripts.js
@@ -1,3 +1,37 @@
 document.querySelector('.hamburger-menu').addEventListener('click', function() {
     document.querySelector('#nav ul').classList.toggle('active');
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+// Optional: add automatic slide show functionality
+let timer = setInterval(function() { plusSlides(1); }, 8000); // Change image every 4 seconds
+
+// Optional: stop the slideshow when the user hovers over the slideshow container
+document.querySelector('.slideshow-container').addEventListener('mouseover', function() {
+    clearInterval(timer);
+});
+
+document.querySelector('.slideshow-container').addEventListener('mouseout', function() {
+    timer = setInterval(function() { plusSlides(1); }, 4000);
+});
